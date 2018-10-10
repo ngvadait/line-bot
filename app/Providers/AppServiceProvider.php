d6945220a5a143b06af549c4150d06916da2364c
ngo.van.dat <FRAMGIA\ngo.van.dat@framgia0404-pc.framgia.com>
2018-10-10 09:47:58 +0700

    test bot line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use LINE\LINEBot;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // LINE BOT
+        $this->app->bind('line-bot', function ($app, array $parameters) {
+            return new LINEBot(
+                new CurlHTTPClient(env('LINE_ACCESS_TOKEN')),
+                ['channelSecret' => env('LINE_CHANNEL_SECRET')]
+            );
+        });
     }
 }
